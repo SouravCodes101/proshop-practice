6284cd75b53ddfd06cabce8232835d38922bca74
Sourav Choudhury <saurabc987@gmail.com>
2023-07-16 17:39:53 +0530

    SetUp Mongo Atlas

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,32 @@
+import { React, useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import Product from "../components/Product.jsx";
+import axios from "axios";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      <Container>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default HomeScreen;
